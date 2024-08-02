ea87323ccc3595ac743b72d2f0aef17d01d04294
guilherme-jardim <guilhermejardimj@gmail.com>
2024-08-02 15:46:57 -0300

    applying Context Selectors

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,7 +1,5 @@
-import { useContext } from 'react'
 import { Header } from '../../components/Header'
 import { dateFormatter, priceFormatter } from '../../utils/formatter'
-import { SearchForm } from '../../components/SearchForm'
 import { Summary } from '../../components/Summary'
 import {
   PriceHighlight,
@@ -9,9 +7,13 @@ import {
   TransactionsTable,
 } from './styles'
 import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { useContextSelector } from 'use-context-selector'
+import { SearchForm } from './components/SearchForm'
 
 export function Transactions() {
-  const { transactions } = useContext(TransactionsContext)
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
 
   return (
     <div>
